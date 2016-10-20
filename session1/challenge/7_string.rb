f1169e0a0177b658e1bc34609966fa4d5f0500ed
mavesonzini <mavesonzini@gmail.com>
2016-10-20 15:56:57 +0100

    Session 1, part 1. commit

---
+++ session1/challenge/7_string.rb
@@ -0,0 +1,14 @@
+# given a string, return the character after every letter "r"
+# 
+# pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+# pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+# pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+
+def pirates_say_arrrrrrrrr(string)
+    new_string = ""
+    string.size.times do |index|
+        if string["r"]
+            new_string << string["r"] + 1
+        end
+    end
+end
