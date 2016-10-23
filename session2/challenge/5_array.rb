1dbcfadf44c6a6b208bd5af38d38c953c6a4fe0c
mavesonzini <mavesonzini@gmail.com>
2016-10-23 12:31:55 +0100

    Session 2, Challenge 5 DONE

--- session2/challenge/5_array.rb
+++ session2/challenge/5_array.rb
@@ -13,3 +13,14 @@
 # mod_three [7]  # => [1]
 #
 # mod_three [0,1,2,3,4,5,6,7] # => [1, 2, 1, 2, 1]
+
+def mod_three(array)
+    new_array = []
+    for number in array
+        get_reminder = number % 3
+            if get_reminder != 0
+                new_array << get_reminder
+            end
+    end
+    return new_array
+end
