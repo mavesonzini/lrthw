88db80abb8f0ccb93a79a7060d90ba16b06226b8
mavesonzini <mavesonzini@gmail.com>
2016-10-23 10:34:03 +0100

    Session 2, challenge 3 DONE

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,11 +7,13 @@
 
 class String
   def every_other_char
-      char = self[i]
+      arr = ""
       for i in 0..self.length - 1
-          if i % 2 == 1
-              puts char
+          char = self[i]
+          if i % 2 == 0
+              arr << char
           end
       end
+      return arr
   end
 end
